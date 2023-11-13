4142f0ff6647f4206aa33f95b13682f9b59ee7b7
Alex Chen <achen119@bu.edu>
2023-11-13 03:19:30 +0000

    added AddressSearch.tsx for creating a dropdown menu of all the major BU dorms through live searching.  Modified textbox.tsx by importing AddressSearch and allowed address textbox to use the dropdown menu function.  Modified page.module.css and added styles for the dropdown menu

---
+++ class-catcher-frontend/src/app/AddressSearch.tsx
@@ -0,0 +1,62 @@
+import React, { useState, ChangeEvent, MouseEvent } from 'react';
+import styles from './page.module.css';
+
+interface AddressSearchProps {
+  onAddressSelected: (address: string) => void;
+}
+
+function AddressSearch({ onAddressSelected }: AddressSearchProps) {
+  const [input, setInput] = useState('');
+  const [filteredAddresses, setFilteredAddresses] = useState<string[]>([]);
+  const addressData = ['Warren Towers', 'West Campus, Claflin Hall', 'West Campus, Sleeper Hall', 
+  'West Campus, Rich Hall', 'Myles Standish Hall', 'Kilachand Hall', 'The Towers', 'Danielsen Hall',
+  '1019 Commonwealth Ave', '575 Commonwealth Ave (Hojo)'];
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const userInput = e.target.value;
+    setInput(userInput);
+
+    // Filter and update filteredAddresses based on userInput
+    const filtered = addressData.filter((address) =>
+      address.toLowerCase().includes(userInput.toLowerCase())
+    );
+    setFilteredAddresses(filtered);
+  };
+
+  const handleAddressClick = (address: string, e: MouseEvent) => {
+    e.preventDefault();
+    onAddressSelected(address);
+    setInput(address);
+    setFilteredAddresses([]); // Close the dropdown when an address is clicked
+  };
+
+  const renderAddresses = () => {
+    if (filteredAddresses.length === 0) {
+      return null;
+    }
+
+    return (
+      <ul className={styles['address-dropdown']}>
+        {filteredAddresses.map((address, index) => (
+          <li key={index} onClick={(e) => handleAddressClick(address, e)}>
+            {address}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  return (
+    <div className={styles['address-search']}>
+      <input
+        type="text"
+        value={input}
+        onChange={handleInputChange}
+        placeholder="Address"
+      />
+      {renderAddresses()}
+    </div>
+  );
+}
+
+export default AddressSearch;
--- class-catcher-frontend/src/app/textbox.tsx
+++ class-catcher-frontend/src/app/textbox.tsx
@@ -1,6 +1,6 @@
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 import styles from './page.module.css'
-
+import AddressSearch from './AddressSearch';
 
 interface TextValues {
   adress: string;
@@ -19,6 +19,11 @@ const CustomTextBox: React.FC = () => {
     setTextValues({ ...textValues, [e.target.name]: e.target.value });
   };
 
+  const handleAddressSelected = (address: string) => {
+    // Set the selected address in the state
+    setTextValues({ ...textValues, adress: address });
+  };
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('Submitted Values:', textValues);
@@ -26,34 +31,26 @@ const CustomTextBox: React.FC = () => {
   };
 
   return (
-    <div>
-        
+    <div className={styles['address-search']}>
       <h1>Class-catcher</h1>
+      <AddressSearch onAddressSelected={handleAddressSelected} />
+      <input
+        type="text"
+        name="text2"
+        value={textValues.text2}
+        onChange={handleTextChange}
+        placeholder="Text 2"
+      />
+      <br />
+      <input
+        type="text"
+        name="text3"
+        value={textValues.text3}
+        onChange={handleTextChange}
+        placeholder="Text 3"
+      />
+      <br />
       <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="adress"
-          value={textValues.adress}
-          onChange={handleTextChange}
-          placeholder="Adress"
-        />
-        <br />
-        <input
-          type="text"
-          name="text2"
-          value={textValues.text2}
-          onChange={handleTextChange}
-          placeholder="Text 2"
-        />
-        <br />
-        <input
-          type="text"
-          name="text3"
-          value={textValues.text3}
-          onChange={handleTextChange}
-          placeholder="Text 3"
-        />
-        <br />
         <button type="submit">Submit</button>
       </form>
     </div>
