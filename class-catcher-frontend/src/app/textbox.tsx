2544ba001eb583e135717fca004a320a53299dbf
Alex Chen <achen119@bu.edu>
2023-11-17 22:34:09 +0000

    added a drop down menu that displays all the collegesin BU. Changed two textboxes to course department and course number respectively.  Course department only allow 2 input values, course number only allow 3 integers. Changed handleTextChange that allows for course number to be only 3 integers inputs

--- class-catcher-frontend/src/app/textbox.tsx
+++ class-catcher-frontend/src/app/textbox.tsx
@@ -4,24 +4,38 @@ import AddressSearch from './AddressSearch';
 
 interface TextValues {
   adress: string;
-  text2: string;
-  text3: string;
+  college: string;
+  courseDepartment: string;
+  courseNumber: string;
 }
 
 const CustomTextBox: React.FC = () => {
   const [textValues, setTextValues] = useState<TextValues>({
     adress: '',
-    text2: '',
-    text3: '',
+    college: '',
+    courseDepartment: '',
+    courseNumber: '',
   });
 
   const handleTextChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTextValues({ ...textValues, [e.target.name]: e.target.value });
+    const { name, value } = e.target;
+
+    if (name === 'courseNumber' && !/^\d{0,3}$/.test(value)) {
+      // Allow only up to 3 digits for courseNumber
+      return;
+    }
+
+    setTextValues({ ...textValues, [name]: value });
   };
 
   const handleAddressSelected = (address: string) => {
     // Set the selected address in the state
     setTextValues({ ...textValues, adress: address });
+  };
+
+  const handleCollegeSelected = (college: string) => {
+    // Set the selected college in the state
+    setTextValues({ ...textValues, college });
   };
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -34,20 +48,63 @@ const CustomTextBox: React.FC = () => {
     <div className={styles['address-search']}>
       <h1>Class-catcher</h1>
       <AddressSearch onAddressSelected={handleAddressSelected} />
+      <br />
+      {/* College Dropdown */}
+      <select
+        name="college"
+        value={textValues.college}
+        onChange={handleTextChange}
+        placeholder="College"
+      >
+        <option value="">Select College</option>
+        <option value="BUA">BUA</option>
+        <option value="CAS">CAS</option>
+        <option value="CDS">CDS</option>
+        <option value="CFA">CFA</option>
+        <option value="CGS">CGS</option>
+        <option value="COM">COM</option>
+        <option value="ENG">ENG</option>
+        <option value="EOP">EOP</option>
+        <option value="FRA">FRA</option>
+        <option value="GMS">GMS</option>
+        <option value="GRS">GRS</option>
+        <option value="HUB">HUB</option>
+        <option value="KHC">KHC</option>
+        <option value="LAW">LAW</option>
+        <option value="MED">MED</option>
+        <option value="MET">MET</option>
+        <option value="OTP">OTP</option>
+        <option value="PDP">PDP</option>
+        <option value="QST">QST</option>
+        <option value="SAR">SAR</option>
+        <option value="SDM">SDM</option>
+        <option value="SED">SED</option>
+        <option value="SHA">SHA</option>
+        <option value="SPH">SPH</option>
+        <option value="SSW">SSW</option>
+        <option value="STH">STH</option>
+        <option value="XAS">XAS</option>
+        <option value="XRG">XRG</option>
+      </select>
+      <br />
+      {/* Class Department Input */}
       <input
         type="text"
-        name="text2"
-        value={textValues.text2}
+        name="courseDepartment"
+        value={textValues.courseDepartment}
         onChange={handleTextChange}
-        placeholder="Text 2"
+        maxLength={2}
+        placeholder="Course Department"
       />
       <br />
+      {/* Course Number Input */}
       <input
         type="text"
-        name="text3"
-        value={textValues.text3}
+        name="courseNumber"
+        value={textValues.courseNumber}
         onChange={handleTextChange}
-        placeholder="Text 3"
+        pattern="\d{3}"
+        placeholder="Course Number"
       />
       <br />
       <form onSubmit={handleSubmit}>
