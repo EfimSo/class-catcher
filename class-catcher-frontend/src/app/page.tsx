ca732e8746e4036a43181f5fcd73ccee20a70ecc
Alex Chen <achen119@bu.edu>
2023-12-03 03:00:05 +0000

    modified page.tsx for displaying sign in with google button

--- class-catcher-frontend/src/app/page.tsx
+++ class-catcher-frontend/src/app/page.tsx
@@ -2,24 +2,23 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import CustomTextBox from './textbox'
+import { AuthProvider, GoogleAuthButton } from './login';
+// Import GoogleLogin from the @react-oauth/google package
+import { GoogleLogin } from '@react-oauth/google';
 
 
 export default function Home() {
 
   return (
-    <main className={styles.main}>
-
-      <div className={styles.center}>
+    <AuthProvider>
+      <main className={styles.main}>
+        <div className={styles.center}>
           <CustomTextBox/>
-
-      </div>
-
-
-      
-          
-
-    </main>
-  )
-
+        </div>
+        <GoogleAuthButton />
+      </main>
+    </AuthProvider>
   
+  );
+
 }
