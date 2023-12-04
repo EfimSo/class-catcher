e18b30f7cbdc1260b3a8358272b84ded1625e991
EfimSo <efim@bu.edu>
2023-12-03 19:18:04 -0500

    API route

--- class-catcher-backend/request_schema.ts
+++ class-catcher-backend/request_schema.ts
@@ -4,10 +4,12 @@ interface request {
     transportMode: string
 }
 
+// Name is a string with the College, Course Department and Course number fields
+
 const example_request = 
     {
-        "name": "CAS CS 350 A1",
-        "address": "123 Example St. OR Warren Towers",
+        "name": "CAS CS 350",
+        "address": "123 Example St", // OR "Warren Towers", will have hardcoded values for big dorms
         "transportMode": "transit"
     }
 
@@ -15,18 +17,17 @@ interface response {
     name: string,
     time: string,
     building: string,
-    distance: number,
-    commute_length: number,
+    distance: string,
+    commute_length: string,
     professor: string
 }
 
-// will have established units for distrance and commute_length that will be the same every time
 const example_response = 
     {
-        'name': "CAS CS 350 A1",
+        'name': "CAS CS 350",
         'time': "MWF 10:10 am-11:00 am",
         'building': "CGS",
-        'distance': 10,
-        'commute_length': 5,
+        'distance': "10 miles",
+        'commute_length': "5 min",
         'professor': "James Johnson"
     }
--- class-catcher-backend/server.py
+++ class-catcher-backend/server.py
@@ -10,7 +10,8 @@ from flask import jsonify, request, json
 from flask_sqlalchemy import SQLAlchemy
 import os.path
 from flask_cors import CORS
-import requests
+# import requests
+import random
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -24,19 +25,25 @@ CORS(app)
 db = SQLAlchemy(app)
 
 
-@app.route('./search')
+@app.route('/search', methods = ['POST'])
 def search():
-    pass
-
-
+    request_data = json.loads(request.data)
+    # task = Class.query.filter_by(class_name=request_data['name']).first() # search for class in db
+    ret_list = [Class(class_name = request_data["name"], class_time = "MWF 10:10 am-11:00 am",
+                      class_building="CGS", class_distance=f"{random.randint(1, 20)} mi",
+                      class_commute_length = f"{random.randint(5, 30)} min",
+                      class_professor = "Perry Dohnam")
+                for _ in range(5)]
+    return jsonify([*map(class_serialize, ret_list)])
 
 class Class(db.Model):
     __tablename__ = 'class'
     id = db.Column(db.Integer, primary_key=True)
     class_name = db.Column(db.Text)
+    class_time = db.Column(db.Text)
     class_building = db.Column(db.Text)
-    class_distance = db.Column(db.Integer)
-    class_commute_length = db.Column(db.Integer)
+    class_distance = db.Column(db.Text)
+    class_commute_length = db.Column(db.Text)
     class_professor = db.Column(db.Text)
     
     def __str__(self):
@@ -45,12 +52,12 @@ class Class(db.Model):
 
 def class_serialize(cl):
     return {
-        'name': cl.name,
-        'time': cl.time,
-        'building': cl.building,
-        'distance': cl.dist,
-        'commute_length': cl.comm_length,
-        'professor': cl.prof,
+        'name': cl.class_name,
+        'time': cl.class_time,
+        'building': cl.class_building,
+        'distance': cl.class_distance,
+        'commute_length': cl.class_commute_length,
+        'professor': cl.class_professor,
     }
 
 @app.route('/')
